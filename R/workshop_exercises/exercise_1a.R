a40986ec47c6ed36d2e14a46cc9dda51f9f3c5f1
matildec <cap.matilde@gmail.com>
2022-09-05 09:10:11 +0200

    Update exercise_1a.R
    + adapted to downloaded data (2018 instead of 2016)
    + added some needed packages
    + set working directory

--- R/workshop_exercises/exercise_1a.R
+++ R/workshop_exercises/exercise_1a.R
@@ -12,35 +12,39 @@
 # Install and load libraries
 
   # Can skip this part if already installed
-  install.packages("survey")
-  install.packages("foreign")
-  install.packages("dplyr")
+  #install.packages("survey")
+  #install.packages("foreign")
+  #install.packages("dplyr")
 
   # Run this part each time you re-start R
   library(survey)
   library(foreign)
   library(dplyr)
+  library(haven)
+  library(tidyverse)
+  library(vctrs)
 
 # Set options to deal with lonely psu
   options(survey.lonely.psu='adjust');
 
+  setwd(folderraw)
 
 # Read in data from 2016 consolidated data file (hc-192)
-  h192 = read.xport("C:/MEPS/h192.ssp")
+  h192 = read_dta("h209.dta")
 
 # Add variables for persons with any expense and persons under 65
 
   h192 = h192 %>%
     mutate(
-      has_exp = (TOTEXP16 > 0), # persons with any expense
+      has_exp = (TOTEXP18 > 0), # persons with any expense
       age_cat = ifelse(AGELAST < 65, "<65", "65+")  # persons under age 65
     )
 
 # QC check on new variables
 
   h192 %>%
-    group_by(has_exp) %>%
-    summarise(min = min(TOTEXP16), max = max(TOTEXP16))
+    group_by(has_exp)     %>%
+    summarise(min = min(TOTEXP18), max = max(TOTEXP18))
 
   h192 %>%
     group_by(age_cat) %>%
@@ -52,19 +56,19 @@
   mepsdsgn = svydesign(
     id = ~VARPSU,
     strata = ~VARSTR,
-    weights = ~PERWT16F,
+    weights = ~PERWT18F,
     data = h192,
     nest = TRUE)
 
 # Overall expenses
-svymean(~TOTEXP16, design = mepsdsgn)
-svytotal(~TOTEXP16, design = mepsdsgn)
+svymean(~TOTEXP18, design = mepsdsgn)
+svytotal(~TOTEXP18, design = mepsdsgn)
 
 # Percentage of persons with an expense
 svymean(~has_exp, design = mepsdsgn)
 
 # Mean expense per person with an expense
-svymean(~TOTEXP16, design = subset(mepsdsgn, has_exp))
+svymean(~TOTEXP18, design = subset(mepsdsgn, has_exp))
 
 # Mean expense per person with an expense, by age category
-svyby(~TOTEXP16, by = ~age_cat, FUN = svymean, design = subset(mepsdsgn, has_exp))
+svyby(~TOTEXP18, by = ~age_cat, FUN = svymean, design = subset(mepsdsgn, has_exp))
